c96bb64a514afbfd7f0376017f7df4779a1a4cd1
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-16 11:50:47 +0100

    created list to store tasks & object model to convert data sent into json

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import UUID, uuid4
 
 # initialize app
 app = FastAPI()
+
+# create list to store task
+tasks = []
+
+# create object to convert data sent to json
+class Task(BaseModel):
+    # specify fields with data
+    id: Optional[UUID] = None
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
 
 # create route and return data to user
 @app.get("/")
